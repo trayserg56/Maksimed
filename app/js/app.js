0e1181684b9a281f7be699621c7ceeefac0eff30
trayserg56 <trayserg@gmail.com>
2022-05-28 19:57:22 +0500

    28.05 до слайдеров

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
-
+	var swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+	  var swiper = new Swiper(".TeamsSwiper", {
+		slidesPerView: 5,
+		spaceBetween: 25,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+		pagination: {
+			el: ".swiper-pagination",
+			type: "progressbar",
+		  },
+      });
+	  var swiper = new Swiper(".NewsSwiper", {
+		slidesPerView: 4.7,
+		spaceBetween: 25,
+		        pagination: {
+          el: ".swiper-kolvo",
+          type: "fraction",
+        },
+		// pagination: {
+		// 	el: ".swiper-pagination",
+		// 	type: "progressbar",
+		//   },
+		  navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		  },
+      });
 })
